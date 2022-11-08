71f5299a5be7b6536e8faa36dc1c7cfe9eee5370
Liu Jiawei <88jliu88@gmail.com>
2022-11-08 16:59:21 -0500

    feat: separate indicators for windows non-visible and unfocused

--- src/ts/apps/focus-checker.ts
+++ src/ts/apps/focus-checker.ts
@@ -2,33 +2,44 @@ import {AreYouFocused} from "../module";
 
 const foundryWindows: Array<Window> = [window];
 
+let isVisible: boolean = true;
 let isFocused: boolean = true;
 
 function emitFocusStatus() {
     const visibleWindow = foundryWindows.find((w) => w.document.visibilityState === "visible");
-    if ((visibleWindow !== undefined) === isFocused)
+    const focusedWindow = foundryWindows.find((w) => w.document.hasFocus());
+    let isNowFocused = focusedWindow !== undefined;
+    let isNowVisible = visibleWindow !== undefined;
+    if (isNowFocused !== isFocused && isNowVisible !== isVisible) // Don't emit if nothing changed
         return;
-    isFocused = !isFocused;
-    game.socket.emit("module.are-you-focused", {"focus": isFocused, "userId": game.user.id});
-    AreYouFocused.log("Emitting focus status", isFocused, visibleWindow);
+    isVisible = isNowVisible;
+    isFocused = isNowFocused;
+    game.socket.emit("module.are-you-focused", {"visible": isVisible, "focus": isFocused, "userId": game.user.id});
+    AreYouFocused.log("Emitting focus status", isVisible, visibleWindow);
 }
 
 Hooks.once("ready", () => {
     if (!game.user.isGM) {
         window.addEventListener("visibilitychange", emitFocusStatus);
+        window.addEventListener("focus", emitFocusStatus);
+        window.addEventListener("blur", emitFocusStatus);
 
         Hooks.on("PopOut:popout", (app: Application, popout: Window) => {
             foundryWindows.push(popout);
             popout.addEventListener("visibilitychange", emitFocusStatus);
+            popout.addEventListener("focus", emitFocusStatus);
+            popout.addEventListener("blur", emitFocusStatus);
         });
 
         Hooks.on("Popout:close", (app: Application, popout: Window) => { // Closed or popped in
             foundryWindows.splice(foundryWindows.indexOf(popout), 1);
         });
     } else { // GM
-        game.socket.on("module.are-you-focused", ({focus, userId}: {focus: boolean, userId: string}) => {
+        game.socket.on("module.are-you-focused", ({visible, focus, userId}: {visible: boolean, focus: boolean, userId: string}) => {
             AreYouFocused.log("Received focus status", focus, "for user", userId);
-            document.querySelector(`#player-list .player[data-user-id="${userId}"]`)?.classList.toggle("unfocused-player", !focus);
+            const playerEl = document.querySelector(`#player-list .player[data-user-id="${userId}"]`) as HTMLElement;
+            playerEl.classList.toggle("unfocused-player", !focus);
+            playerEl.classList.toggle("no-visibility-player", !visible);
         });
     }
 });
